197d026c35d9829e2e6d12e562a2ff9b29571e5e
ManuelInfante <infantemanuel.mail@gmail.com>
2021-09-23 22:06:13 +0200

    keydown, click pallini e timing function - fine

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 Vue.config.devtools = true;
 
+const DURATION = 2 * 1000;
+
 const app = new Vue({
     el: '#app',
     data:{
@@ -9,7 +11,18 @@ const app = new Vue({
             'img/image2.jpg',
             'img/image3.jpg',
             'img/image4.jpg',
-        ]
+        ],
+        playVisible: true,
+        idInterval: null,
+    },
+    mounted() {
+        document.addEventListener('keydown', (e) => {
+            if (e.key == 'ArrowRight'){
+                this.nextPhoto();
+            } else if (e.key == 'ArrowLeft') {
+                this.prevPhoto();
+            }
+        });
     },
     methods: {
         prevPhoto() {
@@ -22,8 +35,25 @@ const app = new Vue({
         nextPhoto() {
             this.counterPhoto += 1;
             if(this.counterPhoto == (this.photos.length)){
-                    this.counterPhoto = 0;
+                this.counterPhoto = 0;
             };
         },
+
+        toggleVisibility() {
+            this.playVisible = !this.playVisible;
+            this.startStopSlider();
+        },
+
+        startStopSlider() {
+            if (!this.playVisible){
+               this.idInterval = setInterval(() => this.nextPhoto(), DURATION);
+            } else {
+                clearInterval(this.idInterval);
+            }
+        },
+
+        changePhoto(index) {
+            this.counterPhoto = index;
+        }
     },
 });
