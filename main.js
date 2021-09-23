46907599a9e3bde81446c91f5b5abdd869121035
ManuelInfante <infantemanuel.mail@gmail.com>
2021-09-23 19:56:44 +0200

    aggiunta funzioni per i tasti e i pallini dinamici

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
+Vue.config.devtools = true;
+
 const app = new Vue({
     el: '#app',
     data:{
-        
+        counterPhoto: 0,
+        photos: [
+            'img/image1.jpg',
+            'img/image2.jpg',
+            'img/image3.jpg',
+            'img/image4.jpg',
+        ]
     },
     methods: {
-        
+        prevPhoto() {
+            this.counterPhoto -= 1;
+            if (this.counterPhoto < 0){
+                this.counterPhoto = (this.photos.length - 1);
+            };
+        },
+
+        nextPhoto() {
+            this.counterPhoto += 1;
+            if(this.counterPhoto == (this.photos.length)){
+                    this.counterPhoto = 0;
+            };
+        },
     },
 });
